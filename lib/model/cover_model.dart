636458bb7954fc3788a470626e55a975850d163e
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-07-24 15:51:06 +0300

    add cover model

---
+++ lib/model/cover_model.dart
@@ -0,0 +1,15 @@
+class CoverModel {
+  final String image ;
+  final String name ;
+
+  CoverModel({required this.image, required this.name});
+} 
+
+    List<CoverModel> covers = [
+
+    CoverModel(name: "Black collection", image: "assets/cover/cover3.png"),
+    CoverModel(name: "HAE BY HAEKIM", image: "assets/cover/cover2.png"),
+    CoverModel(name: "White collection", image: "assets/cover/cover1.png"),
+
+  ];
+
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:opne_fationn/core/color.dart';
 import 'package:opne_fationn/widgets/custem_appar.dart';
 
@@ -11,7 +12,46 @@ class HomePage extends StatelessWidget {
       backgroundColor: AppColor.primary , 
       appBar: CustomAppbar(
         isBlackk: true,
-      ), 
+      ),  
+
+      body: Stack(
+
+        children: [
+
+          // txsts
+          Positioned(
+            top: 18, 
+            left: 10 , 
+            right: 10 , 
+            child: SvgPicture.asset("assets/texts/10.svg"),
+          ) , 
+
+          Positioned(
+            top: 40, 
+            left: 0 , 
+            right: 0 , 
+            child: SvgPicture.asset("assets/texts/October.svg"),
+          ) , 
+          Positioned(
+            top: 85, 
+            left: 0 , 
+            right: 0 , 
+            child: SvgPicture.asset("assets/texts/Collection.svg"),
+          ) , 
+
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 8),
+            child: ListView(
+              children: [
+                SizedBox(height: 150,) , 
+            
+                Image.asset("assets/cover/cover1.png" ,) ,
+                
+              ],
+            ),
+          )
+        ], 
+      ),
       
     ) ;
   }
